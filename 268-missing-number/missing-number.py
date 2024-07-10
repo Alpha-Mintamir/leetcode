51cc4b8a41be43f11474bddedca22346874e8018
Alpha Lencho <133499852+Alpha-Mintamir@users.noreply.github.com>
2024-07-10 21:17:00 +0300

    Time: 97 ms (55.59%) | Memory: 13.7 MB (6.43%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,6 +1,20 @@
 class Solution(object):
     def missingNumber(self, nums):
-        n = len(nums)
-        expected_sum = n * (n + 1) // 2
-        actual_sum = sum(nums)
-        return expected_sum - actual_sum
+        if not nums:
+            return 0
+        maximum = max(nums)
+        minimum = min(nums)
+        
+        full_range = set(range(minimum, maximum + 1))
+        
+        nums_set = set(nums)
+        missing_number = full_range - nums_set
+        
+        if missing_number:
+            return missing_number.pop()
+        
+        
+        if minimum > 0:
+            return minimum - 1
+        else:
+            return maximum + 1
