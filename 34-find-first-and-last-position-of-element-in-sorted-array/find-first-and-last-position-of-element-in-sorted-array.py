1da64f4fd767a3a711e0d912281c21d990c889f6
Alpha Lencho <133499852+Alpha-Mintamir@users.noreply.github.com>
2024-07-15 10:07:46 +0300

    Time: 56 ms (65.22%) | Memory: 12.9 MB (29.50%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,34 +1,35 @@
 class Solution(object):
     def searchRange(self, nums, target):
-        def findStartingIndex(nums, target):
+        def indexFromTheLeft(nums, target):
+            left, right = 0, len(nums)-1
             index = -1
-            low, high = 0, len(nums) - 1
-            while low <= high:
-                mid = (low + high) // 2
-                if nums[mid] >= target:
-                    high = mid - 1
-                else:
-                    low = mid + 1
+            while(left <= right):
+                mid = (left + right)//2
+                if(nums[mid]== target):
+                    index = mid
+                    right = mid -1
+
+                elif(nums[mid]<target):
+                    left = mid + 1
+                elif(nums[mid]>target):
+                    right = mid - 1
+            return index
+        def indexFromTheRight(nums, target):
+            left, right = 0, len(nums) - 1
+            index = -1
+            while left <= right:
+                mid = (left + right) // 2
                 if nums[mid] == target:
                     index = mid
-            return index
-        
-        def findEndingIndex(nums, target):
-            index = -1
-            low, high = 0, len(nums) - 1
-            while low <= high:
-                mid = (low + high) // 2
-                if nums[mid] <= target:
-                    low = mid + 1
+                    left = mid + 1
+                elif nums[mid] < target:
+                    left = mid + 1
                 else:
-                    high = mid - 1
-                if nums[mid] == target:
-                    index = mid
+                    right = mid - 1
             return index
-        
-        start = findStartingIndex(nums, target)
-        end = findEndingIndex(nums, target)
-        
-        return [start, end]
+
+        result = [indexFromTheLeft(nums, target), indexFromTheRight(nums, target)]
+        return result
+    
         
         
