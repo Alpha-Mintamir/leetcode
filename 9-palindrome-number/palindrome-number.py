5ab6f60761763c983c4757e0a12bed45b5834511
Alpha Lencho <133499852+Alpha-Mintamir@users.noreply.github.com>
2024-07-24 14:50:32 +0300

    Time: 46 ms (37.35%) | Memory: 11.7 MB (23.10%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,11 +1,14 @@
 class Solution(object):
     def isPalindrome(self, x):
         x = str(x)
-        k = x[::-1]
-        
-        if k == x:
-            return True
-        else:
-            return False
+        left, right = 0, len(x) -1
+        while left < right:
+            if x[left] == x[right]:
+                left += 1
+                right -= 1
+                continue
+            else:
+                return False
+        return True
 
         
