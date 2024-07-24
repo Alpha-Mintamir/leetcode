d64221bcdace9bff37f0888e143b27b9cb51a5bc
Alpha Lencho <133499852+Alpha-Mintamir@users.noreply.github.com>
2024-07-24 15:16:17 +0300

    Time: 29 ms (63.89%) | Memory: 13.7 MB (31.69%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,10 +1,15 @@
+import re
 class Solution(object):
     def isPalindrome(self, s):
-        k = ''
-        s = s.lower()
-        for i in s:
-            if i.isalnum():
-                k = k + i
-        return k == k[::-1]
-
+        s = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
+        left, right = 0, len(s)-1
+        while left < right:
+            if s[left] == s[right]:
+                left += 1
+                right -= 1
+                continue
+            else:
+                return False
+        return True    
+            
         
