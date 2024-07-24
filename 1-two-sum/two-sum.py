1ddc02ef68c67f919ee976ef836acd1c55211120
Alpha Lencho <133499852+Alpha-Mintamir@users.noreply.github.com>
2024-07-24 14:29:16 +0300

    Time: 36 ms (82.53%) | Memory: 12.5 MB (66.24%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,14 +1,10 @@
-from collections import defaultdict
 class Solution(object):
     def twoSum(self, nums, target):
-        n = len(nums)
-        for i in range(n):
-            for j in range(i+1, n):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        dictt = {}
+        for index, num in enumerate(nums):
+            complement = target - num
+            if complement in dictt:
+                return [index, dictt[complement]]
+            else:
+                dictt[num] = index
 
-            
-        
-
-        
-        
