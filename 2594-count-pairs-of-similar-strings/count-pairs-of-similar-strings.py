7ccb43079db57f75f5ceebaf652e41dafefc1ed3
Alpha Lencho <133499852+Alpha-Mintamir@users.noreply.github.com>
2024-08-07 12:21:05 +0300

    Time: 47 ms (71.15%) | Memory: 11.8 MB (58.33%) - LeetSync

--- 2594-count-pairs-of-similar-strings/count-pairs-of-similar-strings.py
+++ 2594-count-pairs-of-similar-strings/count-pairs-of-similar-strings.py
@@ -1,10 +1,16 @@
 class Solution(object):
     def similarPairs(self, words):
+
+        char_sets = [tuple(sorted(set(word))) for word in words]
+    
+        # Count the number of similar pairs
         count = 0
-        for i in range(len(words)):
-            for j in range(i +1, len(words)):
-                if ''.join(list(sorted(list(set(words[i]))))) == ''.join(list(sorted(list(set(words[j]))))):
+        n = len(words)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if char_sets[i] == char_sets[j]:
                     count += 1
+        
         return count
                     
         
