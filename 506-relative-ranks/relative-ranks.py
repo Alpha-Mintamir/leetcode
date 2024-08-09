9b2ca3e87a739b1ed638aba67ad8ebec841f9e42
Alpha Lencho <133499852+Alpha-Mintamir@users.noreply.github.com>
2024-08-09 10:05:08 +0300

    Time: 37 ms (96.03%) | Memory: 12.8 MB (19.84%) - LeetSync

--- 506-relative-ranks/relative-ranks.py
+++ 506-relative-ranks/relative-ranks.py
@@ -1,17 +1,15 @@
 class Solution:
     def findRelativeRanks(self, score):
-        # Create a dictionary to store the rank based on the sorted score
         medals_list = ["Gold Medal", "Silver Medal", "Bronze Medal"]
         rank_dict = {}
 
-        # Sort the score list in descending order and enumerate over it
         sorted_score = sorted(score, reverse=True)
         for i, s in enumerate(sorted_score):
-            if i < 3:  # Assign medals for the top 3
+            if i < 3:  
                 rank_dict[s] = medals_list[i]
-            else:  # Assign numeric ranks for the rest
+            else:  
                 rank_dict[s] = str(i + 1)
 
-        # Generate the result list based on the original score order
+
         result = [rank_dict[s] for s in score]
         return result
