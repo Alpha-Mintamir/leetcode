0033f8968aaedd017be37657ed5d33307f29f1b8
Alpha Lencho <133499852+Alpha-Mintamir@users.noreply.github.com>
2024-09-09 14:04:52 +0300

    Time: 43 ms (17.26%) | Memory: 17.7 MB (27.89%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -1,39 +1,22 @@
 # Definition for singly-linked list.
-class ListNode(object):
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-class Solution(object):
-    def reverseList(self, head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
-        prev = None
-        curr = head
-        while curr:
-            next_temp = curr.next  
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None  
+        curr = head  
+        while curr is not None:
+            next_node = curr.next 
+            
             curr.next = prev 
             prev = curr 
-            curr = next_temp 
+            curr = next_node  
+
         return prev
+        
+        
 
-
-def create_linked_list(lst):
-    head = ListNode(lst[0])
-    current = head
-    for value in lst[1:]:
-        current.next = ListNode(value)
-        current = current.next
-    return head
-
-
-def linked_list_to_list(node):
-    lst = []
-    while node:
-        lst.append(node.val)
-        node = node.next
-    return lst
-
-
+        
+        
