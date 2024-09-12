3f382c18899baf0a568f017feb4a7e95835181d4
Alpha Lencho <133499852+Alpha-Mintamir@users.noreply.github.com>
2024-09-12 11:59:38 +0300

    Time: 35 ms (56.23%) | Memory: 16.7 MB (8.86%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,28 +1,34 @@
-class Solution(object):
-    def letterCombinations(self, digits):
-        
-        combinations = {"2":"abc", "3":"def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"}
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        num_to_letter = {"2":'abc', '3':'def', '4':'ghi', '5':'jkl', '6': 'mno', '7': 'pqrs', '8':'tuv', '9':'wxyz'}
+        digits = list(digits)
         result = []
-        if len(digits) == 0:
-            return result
-        elif len(digits) == 1:
-            for i in combinations[digits]:
-                result.append(i)
+        if digits == "":
+            return []
+        if len(digits) == 1:
+            return num_to_letter[digits[0]]
         elif len(digits) == 2:
-            for i in combinations[digits[0]]:
-                for j in combinations[digits[1]]:
+            for i in num_to_letter[digits[0]]:
+                for j in num_to_letter[digits[1]]:
                     result.append(''.join([i,j]))
+            return result
         elif len(digits) == 3:
-            for i in combinations[digits[0]]:
-                for j in combinations[digits[1]]:
-                    for k in combinations[digits[2]]:
+            for i in num_to_letter[digits[0]]:
+                for j in num_to_letter[digits[1]]:
+                    for k in num_to_letter[digits[2]]:
+
                         result.append(''.join([i,j,k]))
-        else:
-            for i in combinations[digits[0]]:
-                for j in combinations[digits[1]]:
-                    for k in combinations[digits[2]]:
-                        for l in combinations[digits[3]]:
+            return result
+        elif len(digits) == 4:
+            for i in num_to_letter[digits[0]]:
+                for j in num_to_letter[digits[1]]:
+                    for k in num_to_letter[digits[2]]:
+                        for l in num_to_letter[digits[3]]:
                             result.append(''.join([i,j,k,l]))
-        return result
+            return result
 
 
+
+
+
+        
