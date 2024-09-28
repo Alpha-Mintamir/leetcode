88a2b4560bfde7c3d6ea4be922b6ba85b508d157
Alpha Lencho <133499852+Alpha-Mintamir@users.noreply.github.com>
2024-09-28 11:58:16 +0300

    Time: 166 ms (72.41%) | Memory: 20.9 MB (34.67%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -1,10 +1,11 @@
-class Solution(object):
-    def reverseString(self, s):
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
         left, right = 0, len(s) - 1
         while left < right:
-            s[left], s[right] = s[right], s[left]
+            temp = s[left]
+            s[left] = s[right]
+            s[right] = temp
             left += 1
             right -= 1
-        return s
-
+       
         
