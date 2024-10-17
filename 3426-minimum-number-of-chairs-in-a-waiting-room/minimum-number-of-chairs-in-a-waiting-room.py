3cf5d14c4e25539f5c13f6cb07d9fbb859b55650
Alpha Lencho <133499852+Alpha-Mintamir@users.noreply.github.com>
2024-10-17 23:47:41 +0300

    Time: 36 ms (65.51%) | Memory: 16.6 MB (26.38%) - LeetSync

---
+++ 3426-minimum-number-of-chairs-in-a-waiting-room/minimum-number-of-chairs-in-a-waiting-room.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minimumChairs(self, s: str) -> int:
+        ans = chairs = 0
+        for c in s:
+            chairs+=1 if c=='E' else -1
+            ans = max(ans,chairs)
+        return ans
